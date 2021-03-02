02b72b61d33532c308b88857f01998b4fbc0a333
Sedriskor <78597096+Sedriskor@users.noreply.github.com>
2021-03-02 10:48:42 +0800

    test fix app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_message(event):
     s = '甲霸否'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=))
+        TextSendMessage(text= s)
 
 
 if __name__ == "__main__":
